82f352a372499ef4a95af35efa2b97b1750d704b
seantsang <9394wing@gmail.com>
2024-08-10 15:49:17 +0300

    add dates to event

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -867,6 +867,7 @@ export interface ApiEventEvent extends Schema.CollectionType {
     displayTime_EN: Attribute.String;
     displayTime_CN: Attribute.String;
     order: Attribute.Integer & Attribute.DefaultTo<999>;
+    dates: Attribute.Component<'programs.program-date', true>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
